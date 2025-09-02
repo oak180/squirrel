ed77a20ef5c01c392d4f14f4cb4b23eb2213846d
oak180 <75774346+oak180@users.noreply.github.com>
2025-09-02 22:15:08 +0500

    Template now has fields

--- src/validate.py
+++ src/validate.py
@@ -5,13 +5,6 @@ from pprint import pprint
 import yaml
 
 
-def load_template(template_file: Path) -> dict[str, Any]:
-
-    with open(template_file, 'r') as template_fp:
-        template: dict = yaml.safe_load(template_fp)
-
-    return template
-
 
 def load_asset() -> dict[str, Any]:
 
@@ -43,17 +36,50 @@ def load_asset() -> dict[str, Any]:
     return user_asset
 
 
+
+
+class _TemplateField:
+    def __init__(self, template_field: dict) -> None:
+        self.template_field = template_field
+        self.field_name = list(self.template_field.keys())[0]
+        self.field_props = self.template_field.get(self.field_name)
+        self.field_type = self.field_props.get('type')
+        self.field_desc = self.field_props.get('desc')
+        self.is_mandatory = self.field_props.setdefault('mandatory', False)
+        self.is_required = self.field_props.setdefault('required', False)
+        return
+    
+    def __repr__(self) -> str:
+        s = f'{self.field_name} ({self.field_type}): {self.field_desc}'
+
+        if self.is_mandatory:
+            s += ' (mandatory)'
+        if self.is_required:
+            s += ' (required)'
+
+        return s
+    
+    def prettify(self) -> None:
+        pprint(self.template_field)
+        return
+    
+
+
+
 class Template:
-    def __init__(self, template: dict) -> None:
-        self.template = template
+    def __init__(self, template_dict: dict) -> None:
+        self.template_dict = template_dict
+        self.template_fields = [
+            _TemplateField(f) for f in self.template_dict.get('fields')
+        ]
         return
     
     def prettify(self) -> None:
-        pprint(self.template)
+        pprint(self.template_dict)
         return
     
-    def __repr__(self) -> None:
-        return self.template
+    def __repr__(self) -> str:
+        return self.template_dict
 
     @classmethod
     def from_file(cls, template_path: str) -> Self:
@@ -67,7 +93,10 @@ class Template:
             template = yaml.safe_load(template_fp)
 
         return cls(template)
-        
+
+    def list_fields(self) -> list:
+        return self.template_fields
+
 
 
 
@@ -77,7 +106,13 @@ def validate_asset(template_path: Path) -> None:
     asset = load_asset()
 
     validator.prettify()
-    print(asset)
+    ls_fields = validator.list_fields()
+
+    print(type(ls_fields[0]))
+
+    for each_field in ls_fields:
+        print(each_field)
+    # print(asset)
 
 
 
